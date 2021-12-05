7301c4c946c6e32bf5a4222ee10f6a5a1d99195c
RadSalty <mbsulcs@gmail.com>
2021-12-05 23:15:47 +0800

    minor update before bed

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -3,23 +3,29 @@
 pragma solidity ^0.6.6;
 
 import "@chainlink/contracts/src/v0.6/vendor/SafeMathChainlink.sol"; // uses the brownie-config file to define where @chainlink is
+import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol"; // uses the brownie-config file to define where @chainlink is
+
 
 contract Lottery {
     using SafeMathChainlink for uint256; //inherits safemath for uint256 so functions do not have to be uses explicitly
     
     address public owner; // address of the owner
     address payable[] public players;
+    AggregatorV3Interface public priceFeed; // sets the chainlink interface to pricefeed
 
     // constructor initialises as soon as the contract is deployed
     constructor() public {
+        priceFeed = AggregatorV3Interface(_priceFeed); // sets the priceFeed to the address set in the deploy.py, defined in the brownie-config file
         owner = msg.sender; // sets the deploying wallet as the owner
     }
 
     // Function to enter the lottery
     function enter() public payable {
         // $50 min buy in
+        getEntranceFee()
         players.push(msg.sender);
     }
+
     // Function to get the required entrance fee in USD
     function getEntranceFee public view return(uint256) {
 
