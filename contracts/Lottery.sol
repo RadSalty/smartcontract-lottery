92ac7212dff3deb64c45f2e8a8ddf5ccfe68d9f8
RadSalty <mbsulcs@gmail.com>
2021-12-05 10:27:11 +0800

    expanded functions to create layout

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,10 +1,39 @@
+// SPDX-License-Identifier: MIT
+
 pragma solidity ^0.6.6;
 
 contract Lottery {
 
-    function enter() public {}
-    function getEntranceFee public {}
-    function startLottery() public {}
-    function endLottery() public {}
+    address public owner; // address of the owner
+
+    // constructor initialises as soon as the contract is deployed
+    constructor() public {
+        owner = msg.sender; // sets the deploying wallet as the owner
+    }
+
+    // Function to enter the lottery
+    function enter() public {
+
+    }
+    // Function to get the required entrance fee in USD
+    function getEntranceFee public {
+
+    }
+
+    // creates modifier for owner only functions
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    // an owner only requirement to start the lottery
+    function startLottery() public onlyOwner {
+
+    }
+
+    // an owner only function to end the lottery
+    function endLottery() public onlyOwner {
+
+    }
 
 }
