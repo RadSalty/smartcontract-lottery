bc1c7fe24b717c579266ca2fa896462230121c97
RadSalty <mbsulcs@gmail.com>
2021-12-08 13:22:27 +0800

    finalised contract to begin testing

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -4,14 +4,17 @@ pragma solidity ^0.6.6;
 
 import "@chainlink/contracts/src/v0.6/vendor/SafeMathChainlink.sol"; // uses the brownie-config file to define where @chainlink is
 import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol"; // uses the brownie-config file to define where @chainlink is
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol"; // Uses open zeppelin ownable versions
+import "@chainlink/contracts/src/v0.6/VRFConsumerBase.sol"; // uses chainlink random number
 
-contract Lottery is Ownable {
-    using SafeMathChainlink for uint256; //inherits safemath for uint256 so functions do not have to be uses explicitly
+// inherit ownable & VRFConsumerBase
+contract Lottery is Ownable, VRFConsumerBase {
+    using SafeMathChainlink for uint256; // using safemath for uint256 so functions do not have to be uses explicitly
 
     uint256 public usdEntryFee;
-    address public owner; // address of the owner
     address payable[] public players;
+    address payable public recentWinner;
+    uint256 public randomness;
     AggregatorV3Interface internal ethUsdPriceFeed; // sets the chainlink interface to ethUsdPriceFeed
     enum LOTTERY_STATE {
         OPEN,
@@ -19,13 +22,22 @@ contract Lottery is Ownable {
         CALCULATING_WINNER
     } // 0 = Open, 1=Closed, 2=Calculating winner
     LOTTERY_STATE public lottery_state;
+    uint256 public fee; // fee payable for link random number generator
+    bytes32 public keyhash;
 
     // constructor initialises as soon as the contract is deployed
-    constructor(address _ethUsdPriceFeed) public {
+    constructor(
+        address _ethUsdPriceFeed,
+        address _vrfCoordinator,
+        address _link,
+        uint256 _fee,
+        bytes32 _keyhash
+    ) public VRFConsumerBase(_vrfCoordinator, _link) {
         usdEntryFee = 50 * (10**18); // sets the entry fee to $50USD
         ethUsdPriceFeed = AggregatorV3Interface(_ethUsdPriceFeed); // sets the priceFeed to the address set in the deploy.py, defined in the brownie-config file
-        owner = msg.sender; // sets the deploying wallet as the owner
         lottery_state = LOTTERY_STATE.CLOSED;
+        fee = _fee;
+        keyhash = _keyhash;
     }
 
     // Function to enter the lottery
@@ -56,7 +68,29 @@ contract Lottery is Ownable {
     }
 
     // an owner only function to end the lottery
-    function endLottery() public payable onlyOwner {}
+    function endLottery() public payable onlyOwner {
+        lottery_state == LOTTERY_STATE.CALCULATING_WINNER; // STOPS OTHER FUNCTIONS BEING CALLED
+        bytes32 requestId = requestRandomness(keyhash, fee);
+    }
+
+    // Receives the chainlink random number, set to internal so only VRF co-ordinator can call, override function called to overrise the inherited function
+    function fulfillRandomness(bytes32 _requestId, uint256 _randomness)
+        internal
+        override
+    {
+        require(
+            lottery_state == LOTTERY_STATE.CALCULATING_WINNER,
+            "You aren't there yet!"
+        ); // ensures lotter in the correct state
+        require(_randomness > 0, "random-not-found"); // ensures random number works
+        uint256 indexOfWinner = _randomness % players.length;
+        recentWinner = players[indexOfWinner];
+        recentWinner.transfer(address(this).balance);
+        // Reset lottery
+        players = new address payable[](0);
+        lottery_state = LOTTERY_STATE.CLOSED;
+        randomness = _randomness;
+    }
 
     // Function to update the entry fee minimum
     function updateEntryFee(uint256 _usdEntryFee) public onlyOwner {
